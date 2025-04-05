44ca6d68e354be7bbf8000e645f0711285545cb6
DavidTurnley <94334160+DavidTurnley@users.noreply.github.com>
2025-04-04 23:31:13 -0600

    hopefully fixed the thingy

--- program.py
+++ program.py
@@ -51,9 +51,7 @@ while not userInput.startswith("q") :
 
             #daemonCmd(router, "router ospf")
 
-            cmds = ["Filler"]
-
-            cmds.clear()
+            cmds = ["router ospf"]
 
             cmds.append("ospf router-id 10.0.10" + str(id) + ".1" + str(id))
 
